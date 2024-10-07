ecff3c45127af496c69fb94861c449a3266325ba
Robert Reales <robesistemas@gmail.com>
2024-10-07 11:03:15 -0500

    Eliminados archivos no deseados y actualizado .gitignore

--- src/setupTests.ts
+++
@@ -1,6 +0,0 @@
-// src/setupTests.ts
-
-// Importar las extensiones de jest-dom para mejorar la experiencia de pruebas
-import '@testing-library/jest-dom';
-
-// Aquí puedes agregar más configuraciones de pruebas si es necesario, como mocks globales o configuraciones adicionales.
